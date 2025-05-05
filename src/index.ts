741196c5c596f55ae9a36d57ca4e808cd309748d
BSTluo <1946831552@qq.com>
2025-05-05 15:48:50 +0800

    modified:   package.json 	modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,72 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, Service, h } from 'koishi';
+import * as echarts from 'echarts';
+import { } from 'koishi-plugin-puppeteer';
 
-export const name = 'puppeteer-echarts'
+export const name = 'puppeteer-echarts';
 
-export interface Config {}
+export interface Config { }
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({});
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export default class EchartsServer extends Service
+{
+  inject = ['puppeteer'];
+
+  constructor(ctx: Context)
+  {
+
+    super(ctx, 'echarts');
+    this.ctx = ctx;
+  }
+
+  async renderEchartsImage(width: number, height: number, option: echarts.EChartsOption)
+  {
+    const htmlContent = `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="UTF-8">
+    <style>
+      html, body, #main {
+        margin: 0;
+        padding: 0;
+        width: ${width}px;
+        height: ${height}px;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="main"></div>
+    <script src="https://cdn.jsdelivr.net/npm/echarts@5/dist/echarts.min.js"></script>
+    <script>
+      const chart = echarts.init(document.getElementById('main'));
+      const option = ${JSON.stringify(option)};
+      chart.setOption(option);
+      window.chartRendered = true;
+    </script>
+  </body>
+  </html>`;
+    try
+    {
+      await this.ctx.puppeteer.start();
+      const page = await this.ctx.puppeteer.page();
+      await page.setViewport({ width, height });
+      const over = await this.ctx.puppeteer.render(htmlContent);
+
+      await this.ctx.puppeteer.stop();
+      return over;
+    } catch (err)
+    {
+      await this.ctx.puppeteer.stop();
+      return '图表渲染失败';
+    }
+
+  }
+}
+
+declare module 'koishi' {
+  interface Context
+  {
+    echarts: EchartsServer;
+  }
 }
